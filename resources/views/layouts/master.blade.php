62c6a1548e9538a4528cf071ba742f9ffc5e9cb7
Raymond Loefstop <raymond@be-interactive.nl>
2022-11-29 17:33:06 +0100

    Edit main template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+
+    <title>Kanna</title>
+</head>
+<body>
+@include('partials.navbar')
+
+@yield('content')
+
+@include('partials.footer')
+
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+
+<script>
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+{{--<header>--}}
+{{--    <div class="container">--}}
+{{--        <div class="row" style="margin-left: 10px;">--}}
+{{--            <div id="statusLive" class="airr"></div>--}}
+{{--            <div class="col-md-7">--}}
+{{--                <div class="radio">--}}
+
+{{--                    <audio id="player" id="radio" autoplay style="width: 350px;" volume="20">--}}
+
+{{--                        <source src="https://listen.habbocast.nl/radio/8000/radio.mp3?cache=1665149507" type="audio/mpeg">--}}
+
+{{--                        Your browser does not support the audio element.--}}
+
+{{--                    </audio>--}}
+
+{{--                    <div id="statusArt" class="circle">--}}
+{{--                        <div id="statusDj" class="dj" style="background: transparent url('habbo-avatar-hier') no-repeat scroll 0px 0px; height: 182px; border-radius: 0% 0% 60% 60%; width: 129px;">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+
+{{--                    <div class="play" onclick="document.getElementById('player').play();$('.wave').removeClass('no-animation');">--}}
+{{--                    </div>--}}
+{{--                    <div class="pauze" onclick="document.getElementById('player').pause();waveAfterWave();"></div>--}}
+{{--                    <div class="min" onclick="document.getElementById('player').volume -= 0.1"></div>--}}
+{{--                    <div class="plus" onclick="document.getElementById('player').volume += 0.1"></div>--}}
+
+{{--                    <div class="nummer"></div>--}}
+{{--                    <div class="luisteraar"></div>--}}
+{{--                    <div class="livedj">--}}
+{{--                        <p class="card-text placeholder-glow"><span class="placeholder col-7"></span></p>--}}
+{{--                    </div>--}}
+{{--                    <div class="datum">Vrijdag 7 Oktober 2022</div>--}}
+{{--                    <div class="livenummer">--}}
+{{--                        <p class="card-text placeholder-glow"><span class="placeholder col-7"></span></p>--}}
+{{--                    </div>--}}
+{{--                    <div class="luister">--}}
+{{--                        <p class="card-text placeholder-glow"><span class="placeholder col-7"></span></p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-md-4"><img src="assets/images/hc1.png" style="margin-top: 20px;"></div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</header>--}}
