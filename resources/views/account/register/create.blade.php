7524ce4c96f26a3cb0bc4e947e2261559fed1e42
Raymond Loefstop <raymond@be-interactive.nl>
2022-12-07 17:48:26 +0100

    Few changes, changed locale

--- resources/views/account/register/create.blade.php
+++ resources/views/account/register/create.blade.php
@@ -13,29 +13,34 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Registreren op Kanna</h5>
-
-                        <form>
+                        <form action="{{ route('account.register') }}" method="post">
+                            @csrf
                             <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Gebruikersnaam</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="username" class="form-label">Gebruikersnaam</label>
+                                <input type="text" class="@error('username') is-invalid @enderror form-control" id="username" name="username" value="{{ old('username') }}">
+                                @error('username')
+                                <span style="color: red">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Wachtwoord</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                <label for="password" class="form-label">Wachtwoord</label>
+                                <input type="password" class="@error('password') is-invalid @enderror form-control" name="password" id="password" aria-describedby="passwordHelp">
                                 <div id="passwordHelp" class="form-text">Gebruik nooit je Game wachtwoord!</div>
+                                @error('password')
+                                <span style="color: red">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Wachtwoord herhaling</label>
-                                <input type="password" class="form-control" id="exampleInputPassword2">
+                                <label for="password_repeat" class="form-label">Wachtwoord herhaling</label>
+                                <input type="password" class="@error('password_repeat') is-invalid @enderror form-control" name="password_repeat" id="password_repeat">
+                                @error('password_repeat')
+                                <span style="color: red">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Missie verificatie</label>
-                                <input type="text" class="form-control" id="exampleInputPassword1" value="HD-<?php echo substr(sha1(mt_rand()),17,8); ?>" readonly>
-                                <div id="passwordHelp" class="form-text">Vul deze in je missie in</div>
-                            </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Ik ga akkoord met de algemene voorwaarden</label>
+                                <label for="mottoVerification" class="form-label">Missie verificatie</label>
+                                <input type="text" class="form-control" id="mottoVerification" value="HabboDucket-<?php echo substr(sha1(mt_rand()),17,8); ?>" aria-describedby="mottoHelp" readonly>
+                                <div id="mottoHelp" class="form-text">Vul deze in je missie in</div>
                             </div>
                             <button type="submit" class="btn btn-success">Maak mijn account aan</button>
                             <a href="{{ route('account.login') }}" class="btn btn-secondary" style="width: 120px;">Inloggen</a>
