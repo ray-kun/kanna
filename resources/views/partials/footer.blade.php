707d2b1af983e6daa42d6b068d1ee59564bc2499
Raymond Loefstop <raymond@be-interactive.nl>
2022-12-08 17:33:15 +0100

    footer menu

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,9 +2,10 @@
     <div class="row">
         <div class="col">
             <div class="card">
-                <div class="card-body">
+                <div class="card-body text-center">
                     <h5 class="card-title" style=" text-align: center">&copy; 2022 Kanna. All rechten voorbehouden, Gemaakt met <icon style="color:pink;">♥</icon> door Ryuk</h5>
                     <p style="text-align: center">Wij zijn niet verbonden aan, aanbevolen of gesponsord door of specifiek goedgekeurd door Sulake Corporation Oy of aan haar Gelieerde Ondernemingen. HD kan gebruikmaken van de handelsmerken en andere intellectuele eigendom van Game, zoals toegestaan volgens het Game Fansitebeleid.</p>
+                    <small>Algemene voorwaarden | Voor ouders | Privacybeleid</small>
                 </div>
             </div>
         </div>
